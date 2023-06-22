7ac86da2b24889dee8a11640c2ee6662b65bdcd3
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-06-22 06:29:00 +0100

    did some cleanup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # setup-k8s-dockerless-ubuntu-server
-This repo contains the script to set up a docker-less a multinode K8s on ubuntu servers, related to my Youtube channel presentation: https://youtu.be/H9YfKliGuUY . 
+
+This repo contains the script to set up a docker-less a multinode K8s on ubuntu servers, related to my Youtube channel presentation: https://youtu.be/H9YfKliGuUY .
+
+## References:
+
+https://www.itzgeek.com/how-tos/linux/ubuntu-how-tos/install-containerd-on-ubuntu-22-04.html
--- cluster-setupp-Git.sh
+++ cluster-setupp-Git.sh
@@ -1,10 +1,12 @@
 # ** AWS EC2 Ubuntu
 
-# Spin up Ubuntu 20.04 server on AWS EC2.
+# Spinup two Ubuntu 20.04 server on AWS EC2.
 
-# SSH to the server and run the following command.
-ssh -i <pem file> ubuntu@<public ip>
+# To SSH into the servers, run the following command.
+ssh -i pem_file ubuntu@public_ip
 
+# ** Run the following command on both the servers
+# --------------------------------------------------
 # Update the server
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -13,44 +15,59 @@ sudo apt-get upgrade -y
 sudo apt-get install containerd -y
 
 # Configure containerd and start the service
+# containerd uses a configuration file config.toml for handling its demons.
+# When installing containerd using official binaries, you will not get the configuration file.
+# So, generate the default configuration file using the below commands.
 sudo mkdir -p /etc/containerd
-sudo su -
-containerd config default > /etc/containerd/config.toml
-exit
+containerd config default | sudo tee /etc/containerd/config.toml
 
-# Next, install Kubernetes. First you need to add the repository's GPG key with the command:
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+# If you plan to use containerd as the runtime for Kubernetes,
+# configure the systemd cgroup driver for runC
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+grep SystemdCgroup /etc/containerd/config.toml # Verify the change
 
-# Add the Kubernetes repository
+sudo systemctl restart containerd # Restart containerd
+sudo systemctl status containerd # Verify the status
+
+# -- Next, install Kubernetes --
+# First you need to add the repository's GPG key with the command:
+# Execute following commands to add apt repository for Kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+# Note: At time of writing this guide, Xenial is the latest Kubernetes repository
+# but when repository is available for Ubuntu 22.04 (Jammy Jellyfish) then
+# you need replace xenial word with ‘jammy’ in ‘apt-add-repository’ command.
 
-#Install all of the necessary Kubernetes components with the command:
+# Install all of the necessary Kubernetes components with the following command:
 sudo apt-get install kubeadm kubelet kubectl -y
 
-#Modify "sysctl.conf" to allow Linux Node’s iptables to correctly see bridged traffic
-sudo nano /etc/sysctl.conf
-    # Add this line: net.bridge.bridge-nf-call-iptables = 1
+# Modify "sysctl.conf" to allow Linux Node’s iptables to correctly see bridged traffic
+# refer: https://cloud.tencent.com/developer/article/1828060
+sudo vi /etc/sysctl.conf
+# Add this line: net.bridge.bridge-nf-call-iptables = 1
 
+# Allow packets arriving at the node's network interface to be forwaded to pods.
+# https://linuxconfig.org/how-to-turn-on-off-ip-forwarding-in-linux
 sudo -s
-# Allow packets arriving at the node's network interface to be forwaded to pods. 
 sudo echo '1' > /proc/sys/net/ipv4/ip_forward
+cat /proc/sys/net/ipv4/ip_forward
 exit
 
-# Reload the configurations with the command:
+# Reload the configurations with the command.
 sudo sysctl --system
 
-# Load overlay and netfilter modules 
+# Load overlay and netfilter kernel modules.
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
 # Add other all nodes to hosts file. Change the IP and server names to match your installation. 
 sudo nano /etc/hosts
-    192.168.0.60 ubuntu-server1
-    192.168.0.61 ubuntu-server2
+    172.31.0.93 ubuntu-server1
+    172.31.0.94 ubuntu-master
     
 # Disable swap by opening the fstab file for editing 
 sudo nano /etc/fstab
-    # Comment out "/swap.img"
+# Comment out "/swap.img" if exists
 
 # Disable swap from comand line also 
 sudo swapoff -a
@@ -60,19 +77,18 @@ sudo kubeadm config images pull
 
 ####### This section must be run only on the Master node#############
 
-# **Note: SSH to your master if not already connected.
+# ** Note: SSH to your master if not already connected.
 
-sudo kubeadm init 
+sudo kubeadm init
+
 # Make sure you copy the "kubeadm join" command at the end of above operation, you'lll need to run it on non-master nodes.
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Download Calico CNI
-curl https://docs.projectcalico.org/manifests/calico.yaml > calico.yaml
-# Apply Calico CNI
-kubectl apply -f ./calico.yaml
+# Run following kubectl command to install Calico CNI network plugin from the master node,
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 
 scp -r $HOME/.kube YourID@Non_Master_Node_IP:/home/YourID
 
@@ -82,7 +98,7 @@ exit
 ssh "YourID@Non_Master_Node_IP"
     
 sudo -i 
-    # Paste the token "kubeadm join" command you got from "kubeadm init" operation and run it below
+# Paste the token "kubeadm join" command you got from "kubeadm init" operation and run it below
     
 exit
 
@@ -94,9 +110,6 @@ kubectl cluster-info
 # View nodes (one in our case)
 kubectl get nodes
 
-# Untaint maste
-# kubectl taint node ubuntu-server1 node-role.kubernetes.io/master-
-
 # Schedule a Kubernetes deployment using a container from Google samples
 kubectl create deployment hello-world --image=gcr.io/google-samples/hello-app:1.0
 
@@ -106,48 +119,38 @@ kubectl get deployments
 # Get pod info
 kubectl get pods -o wide
 
-# Scale up the replica set to 4
+# Scale up the replica set
 kubectl scale --replicas=2 deployment/hello-world
 
 # Get pod info
 kubectl get pods -o wide
 
-
-
-
-
-
-
-
-
-#Create a Kubernetes service to expose our service
+# Create a Kubernetes service to expose our service
 kubectl expose deployment hello-world --port=8080 --target-port=8080 --type=NodePort
 
-#Get all deployments in the current name space
+# Get all deployments in the current name space
 kubectl get services -o wide
-
   
 curl http://10.98.39.222:8080
 
-#Test the service using Nodeport
+# Test the service using Nodeport
 curl   http://localhost:32563
 
-#Shell to the pod
+# Shell to the pod
 kubectl exec -it hello-world-5457b44555-cgvtr     -- sh
 exit
 
-#Clean up
+# Clean up
 kubectl delete deployment hello-world
 kubectl delete service hello-world
 
 *******************************************************************************************************
-#Add curl to POD
+# Add curl to POD
 apk --no-cache add curl
 
-#From inside cluster we can do
+# From inside cluster we can do
 curl http://hello-world:8080
-    #rather than ClusterIP
-        curl http://10.99.252.65:8080
+# rather than ClusterIP
+curl http://10.99.252.65:8080
 
-
-#kubeadm reset command. this will un-configure the kubernetes cluster.
+# kubeadm reset command. this will un-configure the kubernetes cluster.
